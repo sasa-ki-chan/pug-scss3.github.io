f7e38ca3a2f69a1ea3c369126988ca8c9091ee61
sasa <udongunma@gmail.com>
2023-12-19 04:14:24 +0900

    gulp設定完了

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,40 @@
-import gulp from 'gulp'
-import notify from 'gulp-notify'
-import plumber from 'gulp-plumber'
-import sass from 'gulp-sass'
-import pug from 'gulp-pug'
-import autoprefixer from 'gulp-autoprefixer'
-import uglify from 'gulp-uglify'
-import browserSync from 'browser-sync'
-import rename from 'gulp-rename'
-import path from 'path'
-import debug from 'gulp-debug'
+import gulp from 'gulp';
+import notify from 'gulp-notify';
+import plumber from 'gulp-plumber';
+// import nodesass from 'node-sass';
+// import sass from 'sass';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass( dartSass );
+import pug from 'gulp-pug';
+import autoPrefixer from 'gulp-autoprefixer';
+import uglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
+import rename from 'gulp-rename';
+import path from 'path';
+import debug from 'gulp-debug';
 
 const paths = {
 	'root'    : './dist/',
-	'pug'     : './src/pug/**/*.pug',
-	'html'    : './dist/**/*.html',
-	'cssSrc'  : './src/scss/**/*.scss',
-	'cssDist'   : './dist/css/',
-	'jsSrc' : './src/js/**/*.js',
-	'jsDist': './dist/js/',
-	'src'    :'./src/'
+	// 'pug'     : './src/pug/**/*.pug',
+	// 'html'    : './dist/**/*.html',
+	// 'cssSrc'  : './src/scss/**/*.scss',
+	// 'cssDist'   : './dist/css/',
+	// 'jsSrc' : './src/js/**/*.js',
+	// 'jsDist': './dist/js/',
+	'src'    :'./src/',
+	src : {
+		'pug' :'./src/pug/**/*.pug',
+		'sass':'./src/scss/**/*.scss',
+		'js'  :'./src/js/**/*.js'
+	},
+
+	dest : {
+		'html' :'./dist/',
+		'css'  :'./dist/css/',
+		'js'   :'./dist/js/'
+	}
+
   }
 
 // gulp.task( 'pug', function() {
@@ -30,30 +46,30 @@ const paths = {
 // 		.pipe( gulp.dest( './' ) );
 // });
 
+  //sass
 gulp.task('sass', function () {
-	console.log("pug");
 	return (
-	  gulp.src(paths.cssSrc)
+	  gulp.src(paths.src.sass)
 		.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-		.pipe(sass({
+		.pipe(sass.sync({
 		  outputStyle: 'expanded'// Minifyするなら'compressed'
 		}))
-		.pipe(autoprefixer({
-		  browsers: ['ie >= 11'],
+		.pipe(autoPrefixer({
+		//   browsers: ['ie >= 11'], //エラーを吐く
 		  cascade: false,
 		  grid: true
 		  }))
 
 		
 		
-		.pipe(gulp.dest(paths.cssDist))
+		.pipe(gulp.dest(paths.dest.css))
 	);
   });
 
   //pug
 gulp.task('pug', function () {
 	return (
-	  gulp.src(['./src/pug/**/*.pug', '!./src/pug/**/_*.pug'], {base: './src/pug/'})
+	  gulp.src([paths.src.pug,'!./src/pug/**/_*.pug'], {base: './src/pug/'})
 		.pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
 		.pipe(pug({
 		  pretty: true
@@ -62,7 +78,7 @@ gulp.task('pug', function () {
         //     path.dirname = './index';
         // }))
 		
-		.pipe(gulp.dest('./dist/'),)
+		.pipe(gulp.dest(paths.dest.html),)
 		.pipe(debug({ title: 'Debug:' })) // 生成されたファイルとディレクトリをコンソールに表示
 	);
   });
@@ -70,10 +86,10 @@ gulp.task('pug', function () {
   // js
   gulp.task('js', function () {
 	return (
-	  gulp.src(paths.jsSrc)
+	  gulp.src(paths.src.js)
 		.pipe(plumber())
 		.pipe(uglify())
-		.pipe(gulp.dest(paths.jsDist))
+		.pipe(gulp.dest(paths.dest.js))
 	);
   });
   
@@ -96,9 +112,9 @@ gulp.task('pug', function () {
   
   //watch
   gulp.task('watch', (done) => {
-	gulp.watch([paths.cssSrc], gulp.series('sass', 'reload'));
-	gulp.watch([paths.jsSrc], gulp.series('js', 'reload'));
-	gulp.watch([paths.pug], gulp.series('pug', 'reload'));
+	gulp.watch([paths.src.sass], gulp.series('sass', 'reload'));
+	gulp.watch([paths.src.js], gulp.series('js', 'reload'));
+	gulp.watch([paths.src.pug], gulp.series('pug', 'reload'));
 	done();
   });
   gulp.task('default', gulp.parallel('watch', 'browser-sync'));
